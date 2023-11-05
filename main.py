0def66834b08e89f42d42711a476a4282b710026
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2023-11-06 00:40:17 +0530

    oh bhai, abhi aur karna hai, final changes before submission

--- main.py
+++ main.py
@@ -47,33 +47,96 @@ def connection(password:str, host:str = 'localhost', user:str = 'root', db:str =
             print("Database does not exist")
         else:
             print(err)
-    
-    
 
-def place_order(cons_name:str, phno:int, list_of_products:dict, address:str = None):   #lop ==>  {proid: number of item, proid2: number of item}
+def place_order(cons_name:str, phno:int, address:str = None):   #lop ==>  {proid: number of item, proid2: number of item}
     #configure the whole 2) place order in this function
     con, cur = connection('myseql', db='shop')
-    #cur = con.cursor()
 
-    cur.execute('select * from inventory')
-    data = cur.fetchall()
-
-    copy_lop = list_of_products.copy()
-    items = {}
-
-    #sorting data to use it for checks
-    for item in data:
-        pid, name, stock, expiry, rate, gst, mnftr = item
-        if stock:
-            items[pid] = [name, stock, expiry, rate, gst, mnftr]
+    items = give_items()
 
     order_id = "REPLACE(REPLACE(REPLACE(SYSDATE(), '-', ''), ':', ''), ' ', '')"
-
-    total_amount = 0
-    base_amount = 0
+    #add sleep(1) or else 2 order can have same order_id
     
     #performing activity using products
-    for product in copy_lop: 
+    print("+++++++++++ SELF BILLING WIZARD +++++++++++")
+    print("b: billing\ne:exit the wizard")
+
+    ordered_item = {}
+
+    while True:
+        print()
+        opid = input("Enter Product ID: ").upper()
+        if opid != 'B' and opid != 'B':
+            if opid in items:
+                q = int(input(f'Quantity for "{items[opid][0]}": '))
+                if q <= items[opid][1]:
+                    #pid = rate, quantity, cost, gst, cost with taxes
+                    rate = items[opid][3]
+                    cost = rate * q
+                    gst = items[opid][4]/100
+                    cost_with_tax = cost + (cost * gst)
+                    ordered_item[opid] = [rate, q, cost, gst, cost_with_tax]
+#!issue date says none
+                else:
+                    print(f"There are only {items[opid][1]} {items[opid][0]} remaining in our stock. Sorry we can't proceed with your request.")
+
+            else:
+                print(f"product id {opid} not found. \nPlease check and enter again.")
+
+        elif opid == "B":
+            total_amount = 0
+            total_amount_with_taxes = 0
+            print("Making you bill. Please wait...")
+            #sleep(1) i think all this computation with account for 1 sec
+            for item in ordered_item:
+                total_amount += ordered_item[item][2]
+                total_amount_with_taxes += ordered_item[item][-1]
+
+            table = []
+            sqlset = {}
+            l = 0
+            for i in ordered_item:
+                tmp = [items[i][0]]
+                sqlset[i] = ordered_item[i][1]
+                l = len(items[i][0])
+                for j in ordered_item[i]:
+                    tmp.append(j)
+
+                table.append(tmp)
+            table.append(["_"*l, "______", "__________", "______", "______", "___________"])
+            table.append(["", "", "Total Cost:", total_amount, "", ""])
+            table.append(["_"*l, "______", "__________", "______", "______", "___________"])
+            table.append(["", "", "", "", "Total Cost\n(including tax)", total_amount_with_taxes])
+
+            header = ["name", "rate", "quantity", "cost", "gst", "cost with taxes"]
+
+            print(tabulate(table, header, tablefmt="pretty"))
+            print()
+            print(f"Grand Total: {total_amount_with_taxes}Rs.")
+
+            i = input("Press enter to continue with payment, c to cancel order")
+            if i != "c":
+                print()
+                print("Payment Done!")
+
+                query = f"INSERT INTO orders VALUES({order_id}, '{cons_name}', {phno}, '{address or None}', '{json.dumps(sqlset)}', {total_amount_with_taxes}, {total_amount})"
+                cur.execute(query)
+                con.commit()
+                print()
+                print("Order placed!")
+                print()
+            
+            return
+
+        else:
+            a = input("are you sure you want to close the wizard?(y/n)").lower()
+            if a == "y":
+                return
+            else:
+                continue
+
+    """
+    for product in list_of_products: 
         if product in items:    #checking if product is listed in database, if stock = 0 then product will not be listed
             if list_of_products[product] <= items[product][1]:   #checking if stock is avaible 
                 #product is avaiable
@@ -91,15 +154,13 @@ def place_order(cons_name:str, phno:int, list_of_products:dict, address:str = No
             list_of_products.pop(product)
             print(f'Product({product}) not found')
 
-    sleep(1)
-
     query = f"INSERT INTO orders VALUES({order_id}, '{cons_name}', {phno}, '{address or None}', '{json.dumps(list_of_products)}', {total_amount}, {base_amount})"
     cur.execute(query)
     con.commit()
 
-    con.close()
+    con.close()"""
 
-def give_data():
+def give_items():
     con, cur = connection('myseql', db='shop')
     #cur = con.cursor()
 
@@ -111,48 +172,34 @@ def give_data():
     #sorting data to use it for checks
     for item in data:
         pid, name, stock, expiry, rate, gst, mnftr = item
-        if stock:
+        if stock:       #this elimates item with no stock
             items[pid] = [name, stock, expiry, rate, gst, mnftr]
 
     return items
 
 def perform_action(ipt):
-    while ipt != '2':
-        if ipt == '1':
-            lst = give_data()
-            header = ["Product ID", "Product Name", "Price"]
-            table = []
-            for product in lst:
-                table.append([product,lst[product][0],lst[product][3]])
+    if ipt == '1':
+        lst = give_items()
+        header = ["Product ID", "Product Name", "Price"]
+        table = []
+        for product in lst:
+            table.append([product,lst[product][0],lst[product][3]])
 
-            print(tabulate(table, header, tablefmt="pretty"))
+        print(tabulate(table, header, tablefmt="pretty"))
 
-        elif ipt == '3':
-            print("Made Using Python🐍, by Hardik")
+    elif ipt == '2':
+        place_order(name, phno, address)
 
-        elif ipt == '4':
-            return
+    elif ipt == '3':
+        print("Made Using Python🐍, by Hardik")
 
-        else:
-            print("Please enter a valid number")
+    elif ipt == '4':
+        return
 
-        ipt = input("Available action(use numbers to select):\n1: View Items\n2: Place Order\n3: Credits\n4: Exit\nAction:")
+    else:
+        print("Please enter a valid number")
 
-    print("To place Order Enter Product Id and then the quantity of that product")
-
-    order = {}
-
-    while True:
-        pid = input("Enter Product Id: ").upper()
-        quantity = int(input(f"Quantity of {give_data()[pid][0]}: "))
-        order[pid] = quantity
-
-        print(pid, quantity)
-
-        ask = input("Enter to Continue, b for billing")
-        if ask == 'b': 
-            place_order(name, phno, order, address)
-            break
+    #ipt = input("Available action(use numbers to select):\n1: View Items\n2: Place Order\n3: Credits\n4: Exit\nAction:")
     
 def admin(): #admin commands, direct access to database and table, eval access for direct interpretation of code
     pswd = input("Enter password: ")
@@ -180,6 +227,9 @@ def admin(): #admin commands, direct access to database and table, eval access f
                 return
 
 if __name__ == '__main__': 
+    print("SHOP MANAGEMENT SYSTEM")
+    print("Made by Hardik Kumar Sinha - XII")
+    print()
     inner_loop = True
     while True:
         while inner_loop:
